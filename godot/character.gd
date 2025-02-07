03b7d607383271e19596b07094592466286ce247
Jordan <jfreyman11037@gmail.com>
2025-02-07 17:55:54 -0500

    fixed button functionality for all menus
    needs more assets - otherwise it crashes

--- godot/character.gd
+++ godot/character.gd
@@ -37,6 +37,8 @@ func update_sprites():
 func set_feature(feature: String, index: int):
 	menu_params[feature] = index
 	match feature:
+		"currBody":
+			bodySprite.texture = $CompositeSprites.body_spritesheet[index]
 		"currEyes":
 			eyesSprite.texture = $CompositeSprites.eyes_spritesheet[index]
 		"currHair":
@@ -46,6 +48,10 @@ func set_feature(feature: String, index: int):
 		"currMouth":
 			mouthSprite.texture = $CompositeSprites.mouth_spritesheet[index]
 
+func _on_view_body_pressed():
+	switch_to = "body"
+	emit_signal("menu_changed", menu_name)
+
 func _on_view_eyes_pressed():
 	switch_to = "eyes"
 	emit_signal("menu_changed", menu_name)
@@ -54,14 +60,31 @@ func _on_view_hair_pressed():
 	switch_to = "hair"
 	emit_signal("menu_changed", menu_name)
 
+func _on_view_nose_pressed():
+	switch_to = "nose"
+	emit_signal("menu_changed", menu_name)
+
+func _on_view_mouth_pressed():
+	switch_to = "mouth"
+	emit_signal("menu_changed", menu_name)
+
 func _on_back_pressed():
 	switch_to = "character"
 	emit_signal("menu_changed", menu_name)
+
+func _on_body_selected(index: int):
+	set_feature("currBody", index)
+
+# Eyes selection function (applies to all eye buttons)
+func _on_eyes_selected(index: int):
+	set_feature("currEyes", index)
 
 # Hair selection function (applies to all hair buttons)
 func _on_hair_selected(index: int):
 	set_feature("currHair", index)
 
-# Eyes selection function (applies to all eye buttons)
-func _on_eyes_selected(index: int):
-	set_feature("currEyes", index)
+func _on_nose_selected(index: int):
+	set_feature("currNose", index)
+
+func _on_mouth_selected(index: int):
+	set_feature("currMouth", index)
