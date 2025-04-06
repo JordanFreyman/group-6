8733a05d6d70f625438e994e6a21346580d5718f
anthonyabruzzo <tgabruzzo@gmail.com>
2025-04-06 04:16:32 -0400

    fixed camera position (covers whole map)
    I changed the camera to be fixed rather than moving because I thought it was a little extra and also I made a couple of the trees have apples

--- godot/camera_2d.gd
+++ godot/camera_2d.gd
@@ -1,11 +1,15 @@
 extends Camera2D
 
 const MOVE_SPEED = 200  
+const SMOOTHING_SPEED = 0.000
 
+
+var min_boundary = Vector2(550, 200) 
+var max_boundary = Vector2(1100, 1050)  
+var target_position: Vector2
 
 func _ready() -> void:
-	pass 
-
+	target_position = global_position
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
@@ -16,3 +20,10 @@ func _process(delta: float) -> void:
 		global_position += Vector2.UP * delta * MOVE_SPEED
 	elif Input.is_action_pressed("ui_down"):
 		global_position += Vector2.DOWN * delta * MOVE_SPEED
+
+	var mouse_pos = get_global_mouse_position()
+
+	target_position = lerp(target_position, mouse_pos, SMOOTHING_SPEED)
+	target_position.x = clamp(target_position.x, min_boundary.x, max_boundary.x)
+	target_position.y = clamp(target_position.y, min_boundary.y, max_boundary.y)
+	global_position = target_position
