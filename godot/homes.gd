d4933aa15732ae96774d54d610f7c266b34b77b6
Jordan <jfreyman11037@gmail.com>
2025-04-09 19:28:13 -0400

    created the homes menu & added name labels

--- godot/homes.gd
+++ godot/homes.gd
@@ -1,9 +1,22 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var homes_node = get_parent()
+	for i in range(Global.islanders.size()):
+		var islander_name = Global.islanders[i]["char_name"]
+		var texture_rect = TextureRect.new()
+		texture_rect.texture = preload("res://Art/house.png")
+		texture_rect.position = Vector2(600 * i, 0)
+		homes_node.add_child(texture_rect)
+
+		var name_label = Label.new()
+		name_label.text = islander_name
+		name_label.label_settings = LabelSettings.new()
+		name_label.label_settings.font_size = 60
+		name_label.label_settings.font_color = "BLACK"
+		name_label.position = Vector2(texture_rect.position.x + 100, texture_rect.position.y + texture_rect.get_rect().size.y - 100)
+		homes_node.add_child(name_label)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
